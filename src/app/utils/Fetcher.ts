adb9ff623b16b18d4a74e222187a44ad4d68696e
Maximus <maximgenov@abv.bg>
2018-09-18 23:29:34 +0300

    Update:  added live server url to "fetcher".

--- src/app/utils/Fetcher.ts
+++ src/app/utils/Fetcher.ts
@@ -4,27 +4,28 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class Fetcher {
-  private db_url: string = 'http://localhost:5000/api'
+  private local_server: string = 'http://localhost:5000/api'
+  private live_server: string = 'https://dress-me-shop.herokuapp.com/api'
 
   constructor(private http: HttpClient) { }
 
   post<T>(collection: string, endpoint: string, data: Object): Observable<T> {
-    const url = `${this.db_url}/${collection}/${endpoint}`
+    const url = `${this.live_server}/${collection}/${endpoint}`
     return this.http.post<T>(url, data)
   }
 
   get<T>(collection: string, endpoint: string): Observable<T> {
-    const url = `${this.db_url}/${collection}/${endpoint}`
+    const url = `${this.live_server}/${collection}/${endpoint}`
     return this.http.get<T>(url)
   }
 
   put<T>(collection: string, endpoint: string, data: Object): Observable<T> {
-    const url = `${this.db_url}/${collection}/${endpoint}`
+    const url = `${this.live_server}/${collection}/${endpoint}`
     return this.http.put<T>(url, data)
   }
 
   delete<T>(collection: string, endpoint: string): Observable<T> {
-    const url = `${this.db_url}/${collection}/${endpoint}`
+    const url = `${this.live_server}/${collection}/${endpoint}`
     return this.http.delete<T>(url)
   }
 }
