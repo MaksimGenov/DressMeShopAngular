41efee170a8a8902af79ffab428acbdd8ee81a69
Maximus <maximgenov@abv.bg>
2019-02-27 20:05:03 +0200

    test

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '/dist/DressMeShop')))
 
-app.get('*', (res, req) => {
-  res.sendFile(path.join(__dirname, '/dist/DressMeShop/index.html'))
+app.use((res, req) => {
+  // res.sendFile(path.join(__dirname, '/dist/DressMeShop/index.html'))
+  res.send('hello')
 })
 
 app.listen(port)
