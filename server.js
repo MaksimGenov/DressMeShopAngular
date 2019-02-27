06b353562fb9660a76611d5f5e8e889d53152746
Maximus <maximgenov@abv.bg>
2019-02-27 20:30:51 +0200

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '/dist/DressMeShop')))
 
-app.use((res, req) => {
+app.get('*', (res, req) => {
   // res.sendFile(path.join(__dirname, '/dist/DressMeShop/index.html'))
   res.send('hello')
 })
