1204b1cc74e3e6063c3618272906f47153022161
Maximus <maximgenov@abv.bg>
2019-02-28 00:07:47 +0200

    test8

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const port = process.env.PORT || 8080
-// const path = require('path')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static('/dist/DressMeShop'))
+app.use(express.static(path.join(__dirname, '/dist/DressMeShop')))
 
-app.get('/*', (res, req) => {
-  res.sendFile('/dist/DressMeShop/index.html')
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/DressMeShop/index.html'))
 })
 
 app.listen(port)
