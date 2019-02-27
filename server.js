9bbece9954c5bc4f734b1c63aa5dbc714b8ca28e
Maximus <maximgenov@abv.bg>
2019-02-28 00:17:06 +0200

    added response compression

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const port = process.env.PORT || 8080
 const path = require('path')
+const compression = require('compression')
 
 const app = express()
 
+app.use(compression())
 app.use(express.static(path.join(__dirname, '/dist/DressMeShop')))
 
 app.get('*', (req, res) => {
